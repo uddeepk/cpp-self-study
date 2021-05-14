8e53641d1a9c536e05f3027eb0ba9f60b0dc18c6
uddeepk <uddeepk@gmail.com>
2021-05-13 21:23:55 -0800

    part 1 done

--- aoc-2015/14/main.cpp
+++ aoc-2015/14/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <ranges>
+#include <algorithm>
+#include <numeric>
+#include <fstream>
 
 struct Reindeer {
     Reindeer(const std::string &s);
@@ -64,26 +68,40 @@ void print(const std::vector<T> &v) {
     }
 }
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
+//
+//    std::string s1 = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.";
+//    std::string s2 = "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+//    Reindeer comet (s1);
+//    Reindeer dancer {s2};
+//    std::cout << comet << "\n";
+//    std::cout << dancer << "\n";
+//
+//    // get input from file
+//    // Testing
+//    std::istringstream iss (s1 + "\n" + s2);
+//    std::vector<Reindeer> vecReindeer = getVecReindeer(iss);
+//
+//    print(vecReindeer);
 
-    std::string s1 = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.";
-    std::string s2 = "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
-    Reindeer comet (s1);
-    Reindeer dancer {s2};
-    std::cout << comet << "\n";
-    std::cout << dancer << "\n";
+//    std::cout << comet.calculateDistance(1000) << "\n";
 
-    // get input from file
-    // Testing
-    std::istringstream iss (s1 + "\n" + s2);
-    std::vector<Reindeer> vecReindeer = getVecReindeer(iss);
+    std::ifstream myFile("input");
 
-    print(vecReindeer);
+    std::vector<Reindeer> vecReindeer = getVecReindeer(myFile);
 
-    std::cout << comet.calculateDistance(1000);
+    std::vector<int> d;
+    int time = 2503;
+    ////// Calculating distance
+    std::ranges::transform(vecReindeer, std::back_inserter(d),
+                                       [&] (const Reindeer &r) -> int {
+        return r.calculateDistance(time); // change this for time
+    });
 
+//    print(d);
 
-
+    std::cout << " star1: " << std::ranges::max(d);
+    //////
     return 0;
 }
 
