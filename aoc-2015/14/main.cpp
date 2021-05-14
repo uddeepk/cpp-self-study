714df077ad94bdcd04f1a3ed48c066afd2ac7bc8
uddeepk <uddeepk@gmail.com>
2021-05-13 20:55:48 -0800

    successful parser test

--- aoc-2015/14/main.cpp
+++ aoc-2015/14/main.cpp
@@ -29,18 +29,34 @@ Reindeer::Reindeer(const string &s) {
 
 std::ostream &operator<<(std::ostream& os, const Reindeer &r) {
     os << r._name << " " << r._flightSpeed << "km/s " << r._flightTime << "s " << r._restTime << "s "
-    << r._cycleTime << "s." << "\n";
+    << r._cycleTime << "s.";
     return os;
 }
 
-
+std::vector<Reindeer> getVecReindeer (std::istream& is) ;
+template <typename T>
+void print(const std::vector<T> &v) {
+    for ( const auto &x : v) {
+        std::cout << x << "\n";
+    }
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
     std::string s1 = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.";
-
+    std::string s2 = "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
     Reindeer comet (s1);
-    std::cout << comet;
+    Reindeer dancer {s2};
+    std::cout << comet << "\n";
+    std::cout << dancer << "\n";
+
+    // get input from file
+    // Testing
+    std::istringstream iss (s1 + "\n" + s2);
+    std::vector<Reindeer> vecReindeer = getVecReindeer(iss);
+
+    print(vecReindeer);
+
     return 0;
 }
 
@@ -52,4 +68,17 @@ std::vector<string> tokenize(const string &s) {
         tokens.push_back(word);
     }
     return tokens;
+}
+
+std::vector<Reindeer> getVecReindeer(std::istream &is) {
+    if ( !is) {
+        std::cerr << "Input Error!";
+        std::exit(-1);
+    }
+    string readBuffer;
+    std::vector <Reindeer> v;
+    while (std::getline(is, readBuffer)) {
+        v.emplace_back(readBuffer);
+    }
+    return v;
 }
